c307e45b71ce20fe1f500b089c113e0dcce5ba27
Oscar Santiago Melgarejo Sandoval <Santiago_Melgarejo@ciencias.unam.mx>
2026-02-26 00:13:27 -0600

    Primer ejercicio Practica 2

---
+++ Practica2/Practica2.hs
@@ -0,0 +1,5 @@
+-- Función sayHello
+-- Recibe una cadena y devuelve "Hello, " concatenado con la cadena y un signo de exclamación
+
+sayHello :: String -> String
+sayHello nombre = "Hello, " ++ nombre ++ "!"
