bb724319974620113f0c8ae2b758c8d5e381b697
Oscar Santiago Melgarejo Sandoval <Santiago_Melgarejo@ciencias.unam.mx>
2026-03-22 23:39:48 -0600

    feat: implementacion de multiplicacion de enteros

--- Practica4/entero.hs
+++ Practica4/entero.hs
@@ -1,3 +1,20 @@
 module Entero where
 
-data Ent = Cero | Suc Ent | Neg Ent deriving (Eq, Show)
+data Entero = Zero | Succ Entero | Neg Entero deriving (Eq, Show)
+
+-- Convierte Entero a Int
+enteroAInt :: Entero -> Int
+enteroAInt Zero = 0
+enteroAInt (Succ n) = 1 + enteroAInt n
+enteroAInt (Neg n) = - enteroAInt n
+
+-- Convierte Int a Entero
+intAEntero :: Int -> Entero
+intAEntero 0 = Zero
+intAEntero n
+    | n > 0 = Succ (intAEntero (n - 1))
+    | n < 0 = Neg (intAEntero (-n))
+
+-- Multiplicación de enteros
+multiEnt :: Entero -> Entero -> Entero
+multiEnt a b = intAEntero (enteroAInt a * enteroAInt b)
