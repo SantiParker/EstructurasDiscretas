31167d3545d5c603f013b184d179eab3fec41e25
Oscar Santiago Melgarejo Sandoval <Santiago_Melgarejo@ciencias.unam.mx>
2026-03-22 23:55:08 -0600

    feat: implementacion de comparacion y aplanado de matrioskas

--- Practica4/mati.hs
+++ Practica4/mati.hs
@@ -1,3 +1,17 @@
 module Mati where
 
 data Matrioska = Mati | Cont Matrioska deriving (Eq, Show)
+
+-- Cuenta la profundidad
+profundidad :: Matrioska -> Int
+profundidad Mati = 0
+profundidad (Cont m) = 1 + profundidad m
+
+-- Compara dos matrioskas
+mayorIgual :: Matrioska -> Matrioska -> Bool
+mayorIgual m1 m2 = profundidad m1 >= profundidad m2
+
+-- Aplana la estructura
+aplana :: Matrioska -> [Matrioska]
+aplana Mati = [Mati]
+aplana m@(Cont x) = m : aplana x
