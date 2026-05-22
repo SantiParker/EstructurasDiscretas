ecd4494ef0c503ca88ed94eda0e6fedadef97f0f
Oscar Santiago Melgarejo Sandoval <Santiago_Melgarejo@ciencias.unam.mx>
2026-05-22 01:54:35 -0600

    fix: agregar funciones faltantes para construir arbol Huffman

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -1,4 +1,5 @@
 module Proyecto where
+import Auxiliar
 
 -- | Tipo de dato que representa un árbol binario de Huffman.
 -- Una hoja almacena un carácter junto con la cantidad de veces
@@ -19,43 +20,6 @@ peso (Hoja _ n) = n
 peso (Nodo n _ _) = n
 
 
--- | Cuenta cuántas veces aparece un carácter específico
--- dentro de una cadena de texto.
--- Recorre la cadena de manera recursiva y suma 1 cada vez
--- que encuentra una coincidencia.
---
--- Ejemplo:
--- contar 'a' "banana" = 3
-contar :: Char -> String -> Int
-contar _ [] = 0
-contar c (x:xs)
-    | c == x = 1 + contar c xs
-    | otherwise = contar c xs
-
-
--- | Elimina caracteres repetidos conservando únicamente
--- una aparición de cada símbolo.
--- Esto es útil para obtener el conjunto base de símbolos
--- sobre el cual se calcularán frecuencias.
---
--- Ejemplo:
--- unicos "banana" = "ban"
-unicos :: String -> String
-unicos [] = []
-unicos (x:xs)
-    | pertenece x xs = unicos xs
-    | otherwise = x : unicos xs
-
-
--- | Determina si un carácter pertenece a una cadena.
--- Devuelve True si encuentra el símbolo y False en caso contrario.
---
--- Esta función es auxiliar para eliminar repeticiones.
-pertenece :: Char -> String -> Bool
-pertenece _ [] = False
-pertenece c (x:xs)
-    | c == x = True
-    | otherwise = pertenece c xs
 
 
 -- | Calcula la frecuencia de aparición de cada símbolo
@@ -65,13 +29,6 @@ pertenece c (x:xs)
 frecuencias :: String -> [(Char, Int)]
 frecuencias xs = ordenar (freqAux (unicos xs) xs)
 
-
--- | Función auxiliar que genera pares
--- (carácter, frecuencia) para cada símbolo único.
-freqAux :: String -> String -> [(Char, Int)]
-freqAux [] _ = []
-freqAux (x:xs) texto =
-    (x, contar x texto) : freqAux xs texto
 
 
 -- | Ordena una lista de frecuencias de mayor a menor
@@ -89,6 +46,24 @@ insertar x [] = [x]
 insertar x (y:ys)
     | snd x > snd y = x:y:ys
     | otherwise = y : insertar x ys
+
+-- Convierte una lista de pares (carácter, frecuencia)
+-- en hojas individuales del árbol Huffman.
+hacerHojas :: [(Char,Int)] -> [Huffman]
+hacerHojas [] = []
+hacerHojas ((c,n):xs) =
+    Hoja c n : hacerHojas xs
+
+
+-- Inserta un árbol dentro de una lista manteniendo
+-- el orden ascendente según su peso.
+-- Esto permite combinar siempre primero los nodos
+-- menos frecuentes durante la construcción Huffman.
+insertarArbol :: Huffman -> [Huffman] -> [Huffman]
+insertarArbol x [] = [x]
+insertarArbol x (y:ys)
+    | peso x <= peso y = x:y:ys
+    | otherwise = y : insertarArbol x ys
 
 -- Ordena una lista de árboles Huffman de menor a mayor
 -- según su peso. Esto es importante porque el algoritmo
